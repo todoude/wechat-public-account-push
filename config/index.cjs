c166d1e7effa7674c965c01475fd2bcea38de071
todoude <117131587+todoude@users.noreply.github.com>
2022-11-01 11:18:47 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,56 +8,58 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx0179f2a004173dc6',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'a5303260145707b9872dbd3da540c2f2',
+  //模板消息id
+  TEMPLATE_ID:"khLpffmMsToJ_NUwBiwpd-lTPr4TCr2wf0dd0FwTXY0''
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '辽宁',
+  CITY: '大连',
 
-  USERS: [
+  USERS: [ toGIe_6kwNRSO005wMNAXX0sZjJhU，oGIe_6qz244IXObZUzs2zaO50SuQ
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '花生猪猪',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'toGIe_6kwNRSO005wMNAXX0sZjJhU，',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '05-27',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '花生猪猪', year: '2003', date: '04-26',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '花生猪猪', year: '2003', date: '05-27',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '在一起纪念日', year: '2020', date: '11-10',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'love_day', date: '2020-11-10' },
+        // 二周年
+        { keyword: '2nd anniver', date: '2022-11-10' },
       ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: ' TEMPLATE_ID:"khLpffmMsToJ_NUwBiwpd-lTPr4TCr2wf0dd0FwTXY0',
 
-  CALLBACK_USERS: [
+  CALLBACK_USERS: [oGIe_6qz244IXObZUzs2zaO50SuQ，oGIe_6kwNRSO005wMNAXX0sZjJhU
     {
-      name: '自己',
+      name: '花生猪猪',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oGIe_6kwNRSO005wMNAXX0sZjJhU',oGIe_6qz244IXObZUzs2zaO50SuQ
     }
   ],
 
